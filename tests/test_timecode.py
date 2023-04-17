9b1494fc4835917d52fab35e389b6df28c672d47
Michael Jordan <michael@glowingpixel.com>
2023-04-17 14:58:21 -0700

    Test some convert()s

--- tests/test_timecode.py
+++ tests/test_timecode.py
@@ -2,12 +2,13 @@ from timecode import Timecode, TimecodeRange
 
 
 tc1 = Timecode("01:00:00:00", rate=24)
-tc2 = Timecode("0:02:30:00", rate=24)
+tc2 = Timecode("0:02:30:15", rate=30)
 
-range = TimecodeRange(start=tc1, duration=tc2)
-print(range.duration, range.end)
-print(range)
-print(Timecode("01:02:00:00") in range)
+print("Timecode 1 is", repr(tc1))
+print("Timecode 2 is", repr(tc2))
 
-for f in range:
-    print(f)
+tc3 = tc1.convert(rate=1)
+tc4 = tc2.convert(rate=1)
+
+print("Timecode 1 resampled is", repr(tc3))
+print("Timecode 2 resampled is", repr(tc4))
