74a885ffc5ba7bbcd5d0d6048d4ca62528d612e9
mjordan <michael@glowingpixel.com>
2023-05-14 00:41:10 -0700

    Coupla theme options here

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,5 +34,10 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+html_theme_options = {
+	'collapse_navigation': False,
+	'style_external_links': False,
+}
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
